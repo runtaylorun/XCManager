fb3f9de201eb30fd6cd6a3b9e411bd8986e9d021
Taylor Mills <trmills@eagles.usi.edu>
2019-08-13 10:10:52 -0400

    Add project files.

---
+++ XCManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XCManager.Startup))]
+namespace XCManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
